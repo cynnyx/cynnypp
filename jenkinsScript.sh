95c0050559f60c728f1a24833943314aeca03bdc
Paolo Monteverde <paolo.monteverde@cynny.com>
2016-04-22 09:18:59 +0200

    set jenkins to build both shared and static libs

--- jenkinsScript.sh
+++ jenkinsScript.sh
@@ -10,6 +10,6 @@ export CXX=/home/jenkins/atlas/bin/g++
 export CC=/home/jenkins/atlas/bin/gcc
 export JENKINS=1
 export LD_LIBRARY_PATH=/home/jenkins/atlas/lib:$LD_LIBRARY_PATH;
-cmake ..
+LIBRARY_TYPE=all cmake ..
 make
 ./test/bin/test_all --reporter junit --out junit_new.xml
