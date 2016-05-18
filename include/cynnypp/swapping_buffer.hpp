fd33d77e7c24df590bd064ecc990524c558f202f
Paolo Monteverde <paolo.monteverde@cynny.com>
2016-05-18 09:02:33 +0200

    added global header file for swapping buffer

---
+++ include/cynnypp/swapping_buffer.hpp
@@ -0,0 +1,8 @@
+#ifndef CYNNYPP_SWAPPING_BUFFER_HPP_
+#define CYNNYPP_SWAPPING_BUFFER_HPP_
+
+#include "io/async/swap/swapping_buffer.h"
+#include "io/async/swap/swapping_buffer_append.h"
+#include "io/async/swap/swapping_buffer_overwrite.h"
+
+#endif // CYNNYPP_SWAPPING_BUFFER_HPP_
