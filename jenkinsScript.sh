5af4cfbc93a54232f54d6d84a13d6c724b191a31
Paolo Monteverde <paolo.monteverde@cynny.com>
2016-09-12 16:14:51 +0200

    fixed (hopefully) jenkins issues

--- jenkinsScript.sh
+++ jenkinsScript.sh
@@ -5,11 +5,3 @@ cd build
 LIBRARY_TYPE=all cmake ..
 make 
 ./test/bin/test_all --reporter junit --out junit.xml
-rm -rf *
-export CXX=/home/jenkins/atlas/bin/g++
-export CC=/home/jenkins/atlas/bin/gcc
-export JENKINS=1
-export LD_LIBRARY_PATH=/home/jenkins/atlas/lib:$LD_LIBRARY_PATH;
-LIBRARY_TYPE=all cmake ..
-make
-./test/bin/test_all --reporter junit --out junit_new.xml
